f61778c2492b6665387cc75645a7eea4bc3103f9
mennaabdelelhady <menamahmod660@gmail.com>
2024-12-03 20:52:52 +0200

    v5_Models

---
+++ .history/app/Models/Store_20241203202017.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mysql';
+
+    protected $table = 'stores';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    public $timestamps = true;
+}
---
+++ .history/app/Models/Store_20241203203105.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mysql';
+
+    protected $table = 'stores';
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'int';
+
+    public $incrementing = true;
+
+    public $timestamps = true;
+}
